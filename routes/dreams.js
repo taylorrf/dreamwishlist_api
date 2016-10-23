514d7d8b996f6c957675f52dcdb423e9716e4156
Tailor Fontela <taylorrf@gmail.com>
2016-10-23 00:57:58 -0200

    updating docs for the get all dreams routes

--- routes/dreams.js
+++ routes/dreams.js
@@ -54,12 +54,33 @@ var dreams = function(app){
    * HTTP/1.1 200 OK
    * [
    *  {
-   *   "id": 1,
-   *   "category": "movie",
-   *   "subcategory": "inception",
-   *   "user_id": 2,
-   *   "createdAt": "2016-10-22T19:08:45.056Z",
-   *   "updatedAt": "2016-10-22T19:08:45.056Z"
+   *    "id": 2,
+   *    "category": "movie",
+   *    "subcategory": "inception",
+   *    "user_id": 1,
+   *    "createdAt": "2016-10-22T21:54:44.308Z",
+   *    "updatedAt": "2016-10-22T21:54:44.308Z",
+   *    "User": {
+   *      "id": 1,
+   *      "name": "John Doe",
+   *      "firebase_key": "123123",
+   *      "photo_url": null,
+   *      "createdAt": "2016-10-22T21:53:03.202Z",
+   *      "updatedAt": "2016-10-22T21:53:03.202Z"
+   *    },
+   *    "Layers": [
+   *      {
+   *        "id": 3,
+   *        "type": "product",
+   *        "description": "Inception DVD",
+   *        "url": "http://assets.ru/product.jpg",
+   *        "product_id": "332",
+   *        "dream_id": 2,
+   *        "user_id": 1,
+   *        "createdAt": "2016-10-23T02:48:22.602Z",
+   *        "updatedAt": "2016-10-23T02:48:22.602Z"
+   *      }
+   *    ]
    *  },
    *  {
    *   "id": 2,
@@ -67,7 +88,16 @@ var dreams = function(app){
    *   "subcategory": "the beach",
    *   "user_id": 2,
    *   "createdAt": "2016-11-22T19:08:45.056Z",
-   *   "updatedAt": "2016-11-22T19:08:45.056Z"
+   *   "updatedAt": "2016-11-22T19:08:45.056Z",
+   *    "User": {
+   *      "id": 1,
+   *      "name": "John Doe",
+   *      "firebase_key": "123123",
+   *      "photo_url": null,
+   *      "createdAt": "2016-10-22T21:53:03.202Z",
+   *      "updatedAt": "2016-10-22T21:53:03.202Z"
+   *    },
+   *    "Layers": []
    *  }
    * ]
    */
